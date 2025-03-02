f047c4371500a64cd7a7dd9efb3a81656ce8a284
AnshMehta24 <ansh.mehta.y24@gmail.com>
2025-03-02 22:34:57 +0530

    ClipPath Added on Landing Page

--- src/components/Sections/LandingPage.tsx
+++ src/components/Sections/LandingPage.tsx
@@ -1,25 +1,61 @@
-"use client"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay} from 'swiper/modules';
-import 'swiper/css';
-import Image from 'next/image';
-import { useState } from 'react';
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import Image from "next/image";
+import { useState } from "react";
 
 const LandingPage = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
   const backgroundImages = [
-    '/bgslides/bg-1.png',
-    '/bgslides/bg-2.png',
-    '/bgslides/bg-3.png',
+    "/bgslides/bg-1.png",
+    "/bgslides/bg-2.png",
+    "/bgslides/bg-3.png",
   ];
-  const slidestext = [
-    'JUNGLE WALKER',
-    'LIFE IS GOOD',
-    'A4 BLACK',
-  ];
+  const slidestext = ["JUNGLE WALKER", "LIFE IS GOOD", "A4 BLACK"];
 
   return (
-    <div className="relative h-screen overflow-hidden">
+    <div className="relative h-screen overflow-hidden ">
+      {/* Top ClipPath with logo */}
+      <div
+        className="absolute -top-1 left-0  z-[20] bg-[#121212] w-full h-full  justify-center items-start  object-cover hidden lg:flex "
+        style={{
+          clipPath:
+            "polygon(0px 0px, 40% 0px, 45% 7%, 55% 7%, 60% 0px, 100% 0px)",
+        }}
+      >
+        <Image
+          src={"/logo.svg"}
+          width={40}
+          height={100}
+          alt="logo"
+          className="object-cover absolute top-2 left-1/2 -translate-x-1/2"
+        />
+      </div>
+      {/* Right ClipPath */}
+      <div
+        className="absolute top-0 right-0 z-[20] bg-[#121212] w-full h-full hidden lg:block "
+        style={{
+          clipPath:
+            "polygon(100% 0px, 100% 25%, 97% 35%, 97% 60%, 100% 70%, 100% 100%, 100% 100%)",
+        }}
+      >
+        <h2 className="w-fit  absolute top-[47%] -translate-y-1/2 -right-12 font-semibold text-xl rotate-90  ">
+          BEST SELLER
+        </h2>
+      </div>
+      {/* Bottom CLipPath */}
+      <div
+        className="absolute top-0 z-[20] right-0 bg-[#121212] text-white w-full h-full hidden lg:block "
+        style={{
+          clipPath: "polygon(100% 100%, 60% 100%, 55% 93%, 45% 93%, 40% 100%)",
+        }}
+      >
+        <h2 className="w-fit  absolute bottom-2 left-1/2 -translate-x-1/2 text-xl   ">
+          BEST SELLER
+        </h2>
+      </div>
+
       {/* Swiper for Background Images */}
       <Swiper
         modules={[Autoplay]}
@@ -47,26 +83,30 @@ const LandingPage = () => {
         ))}
       </Swiper>
 
-     
-        <main className=' absolute inset-0 z-10 min-h-screen h-screen w-full bg-black/30 '>
-        <div className='w-full h-full p-2 flex justify-center items-center relative max-sm:flex-col max-sm:items-start'>
-    <h2 className='absolute left-28 text-9xl font-bold font-coluna mb-10 tracking-wide max-sm:relative max-sm:left-4'>THE <br />ARVAN</h2>
-    <p className='absolute bottom-6 left-6 text-sm w-[30%] font-montserrat max-sm:relative max-sm:w-[90%]'>Ready to level up your comfort game?
-Slip into The Arvan, and feel the difference.
-We’ve got the perfect pair waiting for you.</p>
+      <main className=" absolute inset-0 z-10 min-h-screen h-screen w-full bg-black/30 ">
+        <div className="w-full h-full p-2 flex justify-center items-center relative max-sm:flex-col max-sm:items-start">
+          <h2 className="absolute left-28 text-9xl font-bold font-coluna mb-10 tracking-wide max-sm:relative max-sm:left-4">
+            THE <br />
+            ARVAN
+          </h2>
+          <p className="absolute bottom-6 left-6 text-sm w-[30%] font-montserrat max-sm:relative max-sm:w-[90%]">
+            Ready to level up your comfort game? Slip into The Arvan, and feel
+            the difference. We’ve got the perfect pair waiting for you.
+          </p>
         </div>
-        <button className='px-2 py-1 top-6 left-10 absolute border-2 border-white cursor-pointer w-40 z-50'>
-        
-            
-              <span className="text-center font-montserrat text-white text-md transition-all ">{slidestext[activeIndex]}</span>
-         
+        <button className="px-2 py-1 top-6 left-10 absolute border-2 border-white cursor-pointer w-40 z-50">
+          <span className="text-center font-montserrat text-white text-md transition-all ">
+            {slidestext[activeIndex]}
+          </span>
         </button>
-    </main>
+      </main>
 
-    <div className="absolute bottom-4 right-8   z-10 flex flex-col items-center space-y-2  w-[35vw]">
+      <div className="absolute bottom-4 right-8   z-10 flex flex-col items-center space-y-2  w-[35vw]">
         {/* Slide Index (e.g., 1/3) */}
         <div className="text-white text-sm font-semibold font-montserrat  relative h-3  w-full">
-          <span className='absolute right-10'>{activeIndex + 1}/{backgroundImages.length}</span>
+          <span className="absolute right-10">
+            {activeIndex + 1}/{backgroundImages.length}
+          </span>
         </div>
 
         {/* Progress Indicators (using spans) */}
@@ -75,7 +115,7 @@ We’ve got the perfect pair waiting for you.</p>
             <span
               key={index}
               className={`block w-[30%] h-[3px] rounded-md  transition-colors ${
-                index === activeIndex ? 'bg-white' : 'bg-gray-500'
+                index === activeIndex ? "bg-white" : "bg-gray-500"
               }`}
             ></span>
           ))}
