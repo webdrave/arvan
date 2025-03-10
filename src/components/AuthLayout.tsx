d8cc263d06f00eaf087f3ad571d61918c17b2930
AnshMehta24 <ansh.mehta.y24@gmail.com>
2025-03-11 01:30:18 +0530

    Added Multiple Auth Routes

---
+++ src/components/AuthLayout.tsx
@@ -0,0 +1,46 @@
+"use client";
+import GridLogin from "@/components/GridLogin";
+import Image from "next/image";
+import { ReactNode } from "react";
+import BentoImg from "./BentoImg";
+interface AuthLayoutProps {
+  children: ReactNode;
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white relative overflow-hidden">
+      {/* Back button */}
+      <button
+        // onClick={handleBack}
+        className="absolute top-4 left-4 bg-lime-400 text-black font-bold py-1 px-4 rounded"
+      >
+        Back
+      </button>
+
+      {/* Grid Background Effect */}
+      <div className="absolute inset-0 hidden lg:block">
+        <GridLogin />
+      </div>
+
+      <Image
+        src={"/logo.svg"}
+        width={40}
+        height={80}
+        alt="logo"
+        className="object-cover absolute top-4 right-0 -translate-x-1/2"
+      />
+
+      {/* Main Container */}
+      <div className="flex flex-col lg:flex-row justify-center gap-10 w-full z-10 max-w-6xl h-auto rounded-lg overflow-hidden p-4">
+        <BentoImg />
+
+        <div className="w-full lg:w-1/2 sm:p-10 flex flex-col justify-center ">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
---
+++ src/components/BentoImg.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+const BentoImg = () => {
+  const imageUrl = "/bgslides/bg-1.png";
+  return (
+    <div className="relative w-full max-w-xl aspect-square hidden lg:grid grid-cols-2 gap-2">
+      {/* Top-left large cell (spans 1 row, 1 column) */}
+      <div className="relative overflow-hidden rounded-lg border-2 border-lime-500">
+        <div
+          className="absolute inset-0 bg-cover bg-no-repeat"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundPosition: "0% 0%",
+            backgroundSize: "200% 310%",
+          }}
+        />
+      </div>
+
+      {/* Top-right large cell (spans 2 rows, 1 column) */}
+      <div className="relative overflow-hidden rounded-lg border-2 border-lime-500 row-span-2">
+        <div
+          className="absolute inset-0 bg-cover bg-no-repeat"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundPosition: "100% 0%",
+            backgroundSize: "200% 150%",
+          }}
+        />
+      </div>
+
+      {/* Middle-left small cell */}
+      <div className="relative overflow-hidden rounded-lg border-2 border-lime-500">
+        <div
+          className="absolute inset-0 bg-cover bg-no-repeat"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundPosition: "1% 50%",
+            backgroundSize: "200% 300%",
+          }}
+        />
+      </div>
+
+      {/* Bottom-left large cell (spans 1 row, 1 column) */}
+      <div className="relative overflow-hidden rounded-lg border-2 border-lime-500">
+        <div
+          className="absolute inset-0 bg-cover bg-no-repeat"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundPosition: "0% 100%",
+            backgroundSize: "200% 300%",
+          }}
+        />
+      </div>
+
+      {/* Bottom-right large cell */}
+      <div className="relative overflow-hidden rounded-lg border-2 border-lime-500">
+        <div
+          className="absolute inset-0 bg-cover bg-no-repeat"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundPosition: "100% 100%",
+            backgroundSize: "200% 300%",
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BentoImg;
