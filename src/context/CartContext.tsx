a36783faf6cccd4891ed3565baab50d07173e43a
AnshMehta24 <ansh.mehta.y24@gmail.com>
2025-03-21 00:38:11 +0530

    Added Cart Functionality With Loader on Product Page

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,86 @@
+"use client";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { Products } from "@/components/admin/products-table";
+
+type CartItem = Products & {
+  quantity: number;
+  size: string;
+  color: string;
+};
+
+type cartContext = {
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+  removeFromCart: (id: string) => void;
+  updateQuantity: (id: string, quantity: number) => void;
+  clearCart: () => void;
+};
+
+const CartContext = createContext<cartContext | null>(null);
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  useEffect(() => {
+    const storedCart = localStorage.getItem("cart");
+    if (storedCart) {
+      setCart(JSON.parse(storedCart));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (item: CartItem) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((cartItem) => cartItem.id === item.id);
+      if (existingItem) {
+        return prevCart.map((c) =>
+          c.id === item.id ? { ...c, quantity: c.quantity + item.quantity } : c
+        );
+      }
+
+      return [...prevCart, item];
+    });
+  };
+
+  // Update quantity of an item in the cart
+  const updateQuantity = (id: string, quantity: number) => {
+    setCart((prevCart) =>
+      prevCart.map((cartItem) =>
+        cartItem.id === id ? { ...cartItem, quantity } : cartItem
+      )
+    );
+  };
+
+  const removeFromCart = (id: string) => {
+    setCart((prevCart) => prevCart.filter((cartItem) => cartItem.id !== id));
+  };
+
+  // Clear the entire cart
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addToCart, removeFromCart, updateQuantity, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) throw new Error("useCart must be within a Cart Provider.");
+
+  return context;
+};
